a3b8ae1fb38415ff28282b4039725afd0a5c2b5b
Wendy <wendy@pinterest.com>
2017-12-10 18:43:24 -0800

    Initial Commit

---
+++ TestLex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLex
+//
+//  Created by Wendy Lu on 12/10/17.
+//  Copyright © 2017 Wendy Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestLex/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestLex
+//
+//  Created by Wendy Lu on 12/10/17.
+//  Copyright © 2017 Wendy Lu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
